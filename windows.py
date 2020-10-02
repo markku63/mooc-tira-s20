28b45576bcdc6b8b97ef8b7f35e15ca8e24354b4
Markku Kolkka <markku.kolkka@iki.fi>
2020-10-02 12:32:02 +0300

    Ikkunat ratkaistu paremmin

--- windows.py
+++ windows.py
@@ -1,21 +1,17 @@
 def count(t,k):
     res = []
-    w_beg = 0
-    w_end = k - 1
     numbers = {}
 
-    for i in range(k):
-        numbers[t[i]] = numbers.get(t[i], 0) + 1
-    
-    res.append(len(numbers))
-    while w_end < len(t)-1:       
-        w_end += 1
+    for w_end in range(len(t)):       
         numbers[t[w_end]] = numbers.get(t[w_end], 0) + 1
-        numbers[t[w_beg]] -= 1
-        if numbers[t[w_beg]] == 0:
-            del numbers[t[w_beg]]
-        w_beg += 1
-        res.append(len(numbers))
+        w_beg = w_end - k + 1
+        if w_beg > 0:
+            numbers[t[w_beg - 1]] -= 1
+            if numbers[t[w_beg - 1]] == 0:
+                del numbers[t[w_beg - 1]]
+        if w_beg >= 0:
+            res.append(len(numbers))
+
     return res
 
 if __name__ == "__main__":
