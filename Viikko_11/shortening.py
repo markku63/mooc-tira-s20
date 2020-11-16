21d5813f5f66ad150c2140fef17e32c821e33bb4
Markku Kolkka <markku.kolkka@iki.fi>
2020-11-16 15:28:27 +0200

    Viikko 11 tehtävä 7

--- Viikko_11/shortening.py
+++ Viikko_11/shortening.py
@@ -6,12 +6,35 @@ class Shortening:
         self._n = n
 
     def add_edge(self,a,b,x):
-        self._edges.append((a, b, x))
+        self._edges.append((a-1, b-1, x))
 
     def check(self,a,b):
-        # ajetaan Bellman-Fordia n kierrosta, jos tulos paranee viimeisellä
-        #  kierroksella niin polulla on negatiivinen sykli
-        for i in range(self._n)
+        # ajetaan Bellman-Fordia, jos tulos paranee
+        #  kierroksen n-1 jälkeen niin polulla on negatiivinen sykli
+        distance = [math.inf]*self._n
+        distance[a-1] = 0
+        i = 0
+        while True:
+            i += 1
+            changed = False
+            b_changed = False
+            for edge in self._edges:
+                current = distance[edge[1]]
+                new = distance[edge[0]] + edge[2]
+                if new < current:
+                    distance[edge[1]] = new
+                    if edge[1] == b-1:
+                        b_changed = True
+                    changed = True
+            # print(distance)
+            if not changed: # ei negatiivisia syklejä
+                return False
+            if i >= self._n and b_changed: # negatiivinen sykli b:hen johtavalla polulla
+                return True
+            if i > 2*self._n: # negatiivinen sykli jossain muualla kaaviossa
+                return False
+
+
 
 if __name__ == "__main__":
     s = Shortening(5)
